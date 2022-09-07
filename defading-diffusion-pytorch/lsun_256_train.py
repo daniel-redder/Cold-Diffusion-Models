51d56a9ffa83dd3996be073102aad1c48878b4ed
Daniel Redder <raptersong@gmail.com>
2022-09-07 10:02:26 -0400

    replaced general model with Unet

--- defading-diffusion-pytorch/lsun_256_train.py
+++ defading-diffusion-pytorch/lsun_256_train.py
@@ -38,14 +38,13 @@ parser.add_argument('--residual', action="store_true")
 args = parser.parse_args()
 print(args)
 
-model = Model(resolution=256,
-              in_channels=3,
-              out_ch=3,
-              ch=128,
-              ch_mult=(1, 2, 2, 2),
-              num_res_blocks=2,
-              attn_resolutions=(16,),
-              dropout=0.1).cuda()
+model = Unet(
+    dim=64,
+    dim_mults=(1, 2, 4, 8),
+    channels=3,
+    with_time_emb=not args.remove_time_embed,
+    residual=args.residual
+).cuda()
 
 diffusion = GaussianDiffusion(
     model,
